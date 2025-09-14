f024d73ed41609973740fe297496863c211bc262
Jolly11nth <jollychris131@gmail.com>
2025-09-15 00:49:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react';
-  import path from 'path';
+  import { fileURLToPath } from 'url'
+  import { dirname, resolve } from 'path'
 
+  const __filename = fileURLToPath(import.meta.url)
+  const _dirname = dirname(_filename)
+  
   export default defineConfig({
     plugins: [react()],
     resolve: {
